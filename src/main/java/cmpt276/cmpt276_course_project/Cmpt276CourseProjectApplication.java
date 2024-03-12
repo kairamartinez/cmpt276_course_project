3090f3ac995a214583198e7856409c03bb8e7f9a
Deng Chen <dca150@sfu.ca>
2024-03-12 14:44:49 -0700

    reinitialize

---
+++ src/main/java/cmpt276/cmpt276_course_project/Cmpt276CourseProjectApplication.java
@@ -0,0 +1,13 @@
+package cmpt276.cmpt276_course_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cmpt276CourseProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cmpt276CourseProjectApplication.class, args);
+	}
+
+}
