f5f91a3772b81ec6c8bf41a377bc79fdfdf283e0
Deng Chen <dca150@sfu.ca>
2024-03-12 22:31:19 -0700

    final login logic

--- src/main/java/cmpt276/courseproject/cmpt276_course_project/controllers/UsersController.java
+++ src/main/java/cmpt276/courseproject/cmpt276_course_project/controllers/UsersController.java
@@ -18,13 +18,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestBody;
-
-
 
 @Controller
 public class UsersController {
-    
+
     @Autowired
     private UserRepository usersRepository;
 
@@ -36,54 +33,69 @@ public class UsersController {
         System.out.println("Hello from all users");
         List<User> users = usersRepository.findAll(); // db
         model.addAttribute("users", users);
-        return "users/showAll";
+        return "users/adminPage";
     }
 
     @PostMapping("/users/add")
-    public String addUser(@RequestParam Map<String,String> newUser, HttpServletResponse response) {
+    public String addUser(@RequestParam Map<String, String> newUser, HttpServletResponse response) {
         System.out.println("adding user.");
-        String newName = newUser.get("name");
-        String newPassword = newUser.get("password");
-
-        // default is not admin, to add new admin use database terminal
-        usersRepository.save(new User(newName, newPassword, false));
-        response.setStatus(201);
-        return "Users/Schedule";
+        String newName = newUser.get("newName");
+        String newPassword = newUser.get("newPassword");
+        List<User> userlist = usersRepository.findByNameAndPassword(newName, newPassword);
+        
+        if (userlist.isEmpty()) {
+            // default is not admin, to add new admin use database terminal
+            usersRepository.save(new User(newName, newPassword, false));
+            response.setStatus(201);
+            return "users/Schedule";
+        } else {
+            return "users/planner";
+        }
     }
-    
-    
+
     @GetMapping("/login")
-    public String getLogin(Model model, HttpServletRequest request, HttpSession session){
+    public String getLogin(Model model, HttpServletRequest request, HttpSession session) {
         User user = (User) session.getAttribute("session_user");
-        if (user == null){
-            return "users/login";
-        }
-        else {
-            model.addAttribute("user",user);
-            return "users/protected";
+        if (user == null) {
+            return "users/planner";
+        } else {
+            model.addAttribute("user", user);
+            if (user.isAdmin()) {
+                List<User> users = usersRepository.findAll(); // db
+                model.addAttribute("users", users);
+                return "users/adminPage";
+            } else {
+                return "users/Schedule";
+            }
         }
     }
 
-    @PostMapping("/login")
-    public String login(@RequestParam Map<String,String> formData, Model model, HttpServletRequest request, HttpSession session){
+    @PostMapping("/users/login")
+    public String login(@RequestParam Map<String, String> formData, Model model, HttpServletRequest request,
+            HttpSession session) {
         // processing login
         String name = formData.get("name");
         String pwd = formData.get("password");
         List<User> userlist = usersRepository.findByNameAndPassword(name, pwd);
-        if (userlist.isEmpty()){
-            return "users/login";
-        }
-        else {
+        if (userlist.isEmpty()) {
+            return "users/planner";
+        } else {
             // success
             User user = userlist.get(0);
             request.getSession().setAttribute("session_user", user);
             model.addAttribute("user", user);
-            return "users/Schedule";
+            if (user.isAdmin()) {
+                List<User> users = usersRepository.findAll(); // db
+                model.addAttribute("users", users);
+                return "users/adminPage";
+            } else {
+                return "users/Schedule";
+            }
         }
     }
 
     @GetMapping("/logout")
-    public String destroySession(HttpServletRequest request){
+    public String destroySession(HttpServletRequest request) {
         request.getSession().invalidate();
         return "users/planner";
     }
