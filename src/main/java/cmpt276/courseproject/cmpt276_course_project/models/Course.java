8e6322131fb266ffaa8839e81b0469134ac9a1ce
Deng Chen <dca150@sfu.ca>
2024-04-07 21:14:56 -0700

    clean up

---
+++ src/main/java/cmpt276/courseproject/cmpt276_course_project/models/Course.java
@@ -0,0 +1,19 @@
+package cmpt276.courseproject.cmpt276_course_project.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "courses")
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int courseId;
+    private String courseName;
+    private boolean isComplete;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    // Getters and setters
+}
