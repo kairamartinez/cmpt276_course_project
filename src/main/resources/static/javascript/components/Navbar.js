da41d557bf30185199ea8bb54d790ddd9d852e49
Kaira Martinez <ktm4@sfu.ca>
2024-04-09 18:26:45 -0700

    fixed navbar redirection

--- src/main/resources/static/javascript/components/Navbar.js
+++ src/main/resources/static/javascript/components/Navbar.js
@@ -3,18 +3,18 @@ class Navbar extends HTMLElement {
         this.innerHTML = `
         <nav class="navbar nav navbar-expand-lg">
             <div class="container-fluid">
-                <a class="navbar-brand" href="https://sfussss.org/" target="_blank" onclick="confirmRedirect()">
+                <a class="navbar-brand" href="/">
                     <img src="/assets/icons/sosy-logo.svg" alt="Sosy Logo" class="d-inline-block align-text-top">
-                    SoSy Course Planner
+                    <p>SoSy Course Planner</p>
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon navbar-dark"></span>
                 </button>
                 <div class="justify-content-end collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div class="navbar-nav">
-                        <a class="nav-link" href="/users/listCourses">Progress Checklist</a>
-                        <a class="nav-link" href="/users/scheduled">Schedule Courses</a>
-                        <a class="nav-link" href="https://sfussss.org/" target="_blank" onclick="confirmRedirect()">SoSy Website</a>
+                        <a class="nav-link mt-1" href="/users/listCourses">Progress Checklist</a>
+                        <a class="nav-link mt-1" href="/users/scheduled">Schedule Courses</a>
+                        <a class="nav-link mt-1" href="https://sfussss.org/" target="_blank" onclick="confirmRedirect()">SoSy Website</a>
                         
                         <form action="/users/logout" method="get">
                             <input class="btn button button-green" value="Log Out" type="submit">
